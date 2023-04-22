10c888a06f72554e0bb87f7aa21918cc0475b06d
Tobias Kunze <r@rixx.de>
2023-04-22 16:37:46 +0200

    Add shortcuts

---
+++ setup/shortcuts.ts
@@ -0,0 +1,14 @@
+import type { NavOperations, ShortcutOptions } from '@slidev/types'
+import { defineShortcutsSetup } from '@slidev/types'
+
+export default defineShortcutsSetup((nav: NavOperations, base: ShortcutOptions[]) => {
+  return [
+    ...base, // keep the existing shortcuts
+    {
+      key: 'home',
+      fn: () => nav.go(1),
+      autoRepeat: true,
+    },
+  ]
+})
+
